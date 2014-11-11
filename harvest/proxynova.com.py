af02f14ce06f40c13bf94cd00911bec50ea51c8c
jzk <jzk@freenode.memoserv>
2014-11-11 23:10:53 +0000

    Add harvester for proxynova.

---
+++ harvest/proxynova.com.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from Harvester import WebParser
+
+url          = "http://www.proxynova.com/proxy-server-list/"
+ip_regex     = r'\<span class\=\"row_proxy_ip\"\>\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s*\</span\>'
+port_regex   = r'(?:left\"\>\s+|\d{2,5}\"\>)(\d{2,5})(?:\s+\</td\>|\</a\>)'
+
+wp = WebParser(url,(ip_regex,port_regex))
+
+for remote in wp.remotes():
+	wp.scan(remote)
