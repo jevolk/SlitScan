650704d93f690e30ecd0fa5f1d79b362a8a2b8ef
jzk <jzk@freenode.memoserv>
2014-11-12 01:12:41 +0000

    Add preliminary log analyzer.

---
+++ loganal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys
+from collections import namedtuple
+from harvest.Harvester import Harvester
+
+
+if len(sys.argv) < 2:
+	print "usage: %s <logfile>" % sys.argv[0]
+	exit(-1)
+
+
+harvester = Harvester()
+column = namedtuple("Fields","time, symbol, fd, mask, http_code, state_code, state, remote")
+
+for row in open(sys.argv[1]):
+	colv, reason = row.split(" : ")
+	elem = column._make(colv.split(" "))
+
+	if elem.symbol == "()":
+		print "%d %s" % (float(elem.time),elem.remote)
+		harvester.scan(elem.remote)
